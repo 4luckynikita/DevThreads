e854a48727eabe6938b16ff84e2bc5cdf3849507
nikita <4luckynikita@gmail.com>
2024-06-07 11:47:53 -0700

    attempt 2

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,9 +1,8 @@
-from .db import db
+from .db import db, environment, SCHEMA
 from .user import User
-from .cartitem import CartItem
-from .distributor import Distributor
 from .item import Item
+from .cartitem import CartItem
 from .order import Order
 from .orderitem import OrderItem
 from .review import Review
-from .db import environment, SCHEMA
+from .distributor import Distributor

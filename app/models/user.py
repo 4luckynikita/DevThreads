ec53a9870c13ec51b9333f3b0009ba28631e3ce6
nikita <4luckynikita@gmail.com>
2024-06-07 12:24:48 -0700

    mmmm

--- app/models/user.py
+++ app/models/user.py
@@ -23,7 +23,7 @@ class User(db.Model, UserMixin):
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
     updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
 
-    # cart_items = db.relationship("CartItem", back_populates="user", cascade="all, delete-orphan")
+    cart_items = db.relationship("CartItem", back_populates="user", cascade="all, delete-orphan")
     orders = db.relationship("Order", back_populates="user", cascade="all, delete-orphan")
     reviews = db.relationship("Review", back_populates="user", cascade="all, delete-orphan")
     distributor = db.relationship("Distributor", back_populates="user", cascade="all, delete-orphan")

a1b2d579cf95763ebed18c95dfcdf85d1d5aacc1
nikita <4luckynikita@gmail.com>
2024-10-23 20:58:22 -0700

    updated about image

--- react-vite/src/components/AboutPage/AboutPage.jsx
+++ react-vite/src/components/AboutPage/AboutPage.jsx
@@ -4,7 +4,7 @@ const AboutPage = () => {
     return <div className="about-container">
         <h1 className="about-container-heading">About</h1>
         <div className="about-inner">
-            <img src="https://media.licdn.com/dms/image/D5603AQGh3kLsso8YHA/profile-displayphoto-shrink_200_200/0/1697774614868?e=1723680000&v=beta&t=2NyHsuGQHfGZFxFXu-22ev-pK-Ah7RTvFyeMA8W-QQE" className="about-img" />
+            <img src="https://asset.cloudinary.com/dkxfjbynk/b9807e70c98852986ec1d34229afdd67" className="about-img" />
             <h2>DevThreads was built by Nikita Kastyshyn</h2>
             <a href="mailto:4luckynikita@gmail.com">4luckynikita@gmail.com</a>
             <p className="hello-why-r-u-here">Check out the repo and Nikita's LinkedIn below:</p>
